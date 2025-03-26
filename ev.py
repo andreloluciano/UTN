9fc6a0ee7269124e283dc6577ef41a43dbf99e2e
Luciano <andreloluciano91@gmail.com>
2025-03-25 21:28:14 -0300

    Testing

---
+++ ev.py
@@ -0,0 +1,21 @@
+# a = 1
+# b = 2
+# c = 4
+
+# A = False
+# B = False
+# C = True
+
+# r1 = (2 * b - 1) / (2 * a) > 3
+# r2 = a * (b * c) >= 30 and not((a + b) * c >= (350 * c))
+# r3 = A and r1 or B == r2
+# r4 = A and B and r3 and C != r1
+
+# print(f"{r1}, {r2}, {r3}, {r4}")
+
+a = 10
+b = 3.0
+c = a * b
+d = a + b
+
+print(f"c = {c}, d = {d}")
